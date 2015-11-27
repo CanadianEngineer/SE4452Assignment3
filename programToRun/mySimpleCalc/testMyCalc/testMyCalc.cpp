514c4f14192730669c0f8f38ac7880921cf94972
Long Pham <longish222@gmail.com>
2015-11-27 15:33:54 -0500

    Added Project To Run
    the following MS VS project contains a simple calculator, google test,
    and a simple test case

---
+++ programToRun/mySimpleCalc/testMyCalc/testMyCalc.cpp
@@ -0,0 +1,43 @@
+// testMyCalc.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "gtest/gtest.h"
+#include "mySimpleCalc.h"
+
+
+TEST(testCalc, testAdd)
+{
+	Calculator c;
+	EXPECT_EQ(2,c.Add(1,1));
+//	system("pause");
+}
+
+TEST(testCalc, testSub)
+{
+	Calculator c;
+	EXPECT_EQ(1,c.Subtract(1,1));
+	//system("pause");
+}
+
+TEST(testCalc, testMul)
+{
+	Calculator c;
+	EXPECT_EQ(3,c.Multiply(1,3));
+//	system("pause");
+}
+
+TEST(testCalc, empty)
+{
+
+//	system("pause");
+}
+
+
+/*
+int _tmain(int argc, _TCHAR* argv[])
+{
+	testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+*/
